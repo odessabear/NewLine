2f986d7e6c493c254d0eeab577cbf138f20ec59e
odessabear@gmail.com <odessabear@gmail.com>
2019-07-05 13:54:50 +0300

    OOP course on java ,exercise 3 shop with inheritance and composition.

--- src/com/amikhailov/oop/shop/SalesRoom.java
+++ src/com/amikhailov/oop/shop/SalesRoom.java
@@ -1,8 +1,16 @@
 package com.amikhailov.oop.shop;
 
+import com.amikhailov.oop.shop.client.BaseVisitor;
+import com.amikhailov.oop.shop.department.BaseDepartment;
+import com.amikhailov.oop.shop.service.Administrator;
+
+import java.util.ArrayList;
+
 public class SalesRoom {
 
     private String name;
-    private String consultants;
+    private ArrayList<BaseDepartment> departmentList;
+    private ArrayList<BaseVisitor> visitorList;
+    private Administrator administrator;
 
 }
